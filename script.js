bbd407392bfbd9297b9f2e8fd4d7d6e5f96b9a4f
Anwender <florianzunke@gmx.de>
2026-08-12 21:01:44 +0200

    update generateToolList funktion

--- script.js
+++ script.js
@@ -3,13 +3,40 @@ let packButton = document.getElementById("btn-packliste");
 packButton.addEventListener("click", generateToolList);
 
 function generateToolList() {
+    let outputList = document.getElementById("output-list");
     let checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
     let selectedTasks = [];
     checkboxes.forEach(checkbox => {
         selectedTasks.push(checkbox.value);
     });
 
-    console.log(packButton, "Button clicked!");
-    console.log(checkboxes, "Selected tasks!");
-    console.log(selectedTasks, "Selected tasks array!");
+    let allTools = [];
+
+    selectedTasks.forEach(taskKey => {
+        let taskTools = database[taskKey].tools;
+
+        if (taskTools) {
+            allTools.push(...taskTools);
+        }
+    });
+
+    let filteredTools = allTools.filter((tool, index) => {
+        return allTools.indexOf(tool) === index;
+    });
+
+    outputList.innerHTML = "";
+    filteredTools.forEach(tool => {
+        let listItem = document.createElement("li");
+        let checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.id = `tool-${tool}`;
+        checkbox.value = tool;
+        let label = document.createElement("label");
+        label.htmlFor = `tool-${tool}`;
+        label.textContent = " " + tool;
+
+        listItem.appendChild(checkbox);
+        listItem.appendChild(label);
+        outputList.appendChild(listItem);
+    });
 }
--- scripts/db.js
+++ scripts/db.js
@@ -1,20 +1,20 @@
-let window_installation = {
-    "tools": ["Bohrhammer", "Flex", "E-Hobel", "Panzer", "Tauchsäge", "Multimaster", "Wassserwaagen", "Akkuschrauber", "privates Werkzeug"],
-    "materials": ["Fenster", "Montageschaum", "Dichtband", "Sprühprimer", "Kombriband", "trioplex", "Acryl", "Montagekleber", "schrauben", "Glasreiniger", "IndustrieClean", "Schaumreiniger", "Farbe"]
+database = {
+    window_installation: {
+        "tools": ["Bohrhammer", "Flex", "E-Hobel", "Panzer", "Tauchsäge", "Multimaster", "Wassserwaagen", "Akkuschrauber", "privates Werkzeug"],
+        "materials": ["Fenster", "Montageschaum", "Dichtband", "Sprühprimer", "Kombriband", "trioplex", "Acryl", "Montagekleber", "schrauben", "Glasreiniger", "IndustrieClean", "Schaumreiniger", "Farbe"]
+    },
+    indoor_installation: {
+        "tools": ["Bohrhammer", "Flex", "E-Hobel", "Panzer", "Tauchsäge", "Multimaster", "Wassserwaagen", "Akkuschrauber", "privates Werkzeug"],
+        "materials": ["Tür", "Montageschaum", "Acryl", "Montagekleber", "schrauben", "IndustrieClean", "Farbe"]
+    },
+    main_entrance_door_installation: {
+        "tools": ["Bohrhammer", "Flex", "E-Hobel", "Panzer", "Tauchsäge", "Multimaster", "Wassserwaagen", "Akkuschrauber", "privates Werkzeug"],
+        "materials": ["Tür", "Montageschaum", "Dichtband", "Sprühprimer", "Kombriband", "trioplex", "Acryl", "Montagekleber", "schrauben", "IndustrieClean", "Schaumreiniger", "Farbe"]
+    },
+    restoration: {
+        "tools": ["Flex", "E-Hobel", "Panzer", "Tauchsäge", "Multimaster", "Oberfräse", "mobile Tischkreissäge", "Akkuschrauber", "privates Werkzeug"],
+        "materials": ["Leim", "Acryl", "Montagekleber", "schrauben", "Farbe"]
+    }
 }
 
-let indoor_installation = {
-    "tools": ["Bohrhammer", "Flex", "E-Hobel", "Panzer", "Tauchsäge", "Multimaster", "Wassserwaagen", "Akkuschrauber", "privates Werkzeug"],
-    "materials": ["Tür", "Montageschaum", "Acryl", "Montagekleber", "schrauben", "IndustrieClean", "Farbe"]
-}
 
-let 
-    main_entrance_door_installation = {
-    "tools": ["Bohrhammer", "Flex", "E-Hobel", "Panzer", "Tauchsäge", "Multimaster", "Wassserwaagen", "Akkuschrauber", "privates Werkzeug"],
-    "materials": ["Tür", "Montageschaum", "Dichtband", "Sprühprimer", "Kombriband", "trioplex", "Acryl", "Montagekleber", "schrauben", "IndustrieClean", "Schaumreiniger", "Farbe"]
-}
-
-let restoration = {
-    "tools": ["Flex", "E-Hobel", "Panzer", "Tauchsäge", "Multimaster", "Oberfräse","mobile Tischkreissäge", "Akkuschrauber", "privates Werkzeug"],
-    "materials": ["Leim", "Acryl", "Montagekleber", "schrauben", "Farbe"]
-}
