a443147177c6731cb026cafc76b946491208c114
Anwender <florianzunke@gmx.de>
2026-08-13 19:27:27 +0200

    add function to add Single tool

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let packButton = document.getElementById("btn-packliste");
+let extraToolButton = document.getElementById("btn-add-extra-tool");
 
 packButton.addEventListener("click", generateToolList);
+extraToolButton.addEventListener("click", addSingleTool);
 
 function generateToolList() {
     let outputList = document.getElementById("output-list");
@@ -28,10 +30,12 @@ function generateToolList() {
     filteredTools.forEach(tool => {
         let listItem = document.createElement("li");
         let checkbox = document.createElement("input");
+        let label = document.createElement("label");
+        
         checkbox.type = "checkbox";
         checkbox.id = `tool-${tool}`;
         checkbox.value = tool;
-        let label = document.createElement("label");
+        
         label.htmlFor = `tool-${tool}`;
         label.textContent = " " + tool;
 
@@ -39,4 +43,29 @@ function generateToolList() {
         listItem.appendChild(label);
         outputList.appendChild(listItem);
     });
+}
+
+function addSingleTool() {
+    let inputFieldExtraTool = document.getElementById("input-extra-tool");
+    let extraTool = inputFieldExtraTool.value.trim();
+
+    if (inputFieldExtraTool.value !== "") {
+        let outputList = document.getElementById("output-list");
+        let listItem = document.createElement("li");
+        let checkbox = document.createElement("input");
+        let label = document.createElement("label");
+
+        checkbox.type = "checkbox";
+        checkbox.id = `tool-${extraTool}`;
+        checkbox.value = extraTool; 
+
+        label.htmlFor = `tool-${extraTool}`;
+        label.textContent = " " + extraTool;    
+
+        listItem.appendChild(checkbox);
+        listItem.appendChild(label);
+        outputList.appendChild(listItem);   
+
+        inputFieldExtraTool.value = "";
+    }
 }
